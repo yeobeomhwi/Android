51cca83d9c05afa0d631060e2a77787b88c00a5d
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-04-03 17:36:41 +0900

    .

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.css);
+        getSupportActionBar().setIcon(R.drawable.html5);
     }
 }
