c577a9f009258df5aae0fd21ae07091e76d49091
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-05-22 16:50:39 +0900

    [챕터8]

---
+++ Chapter 8/Project8_3/app/src/main/java/com/example/project8_3/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.project8_3;
+
+import java.io.File;
+import java.util.Arrays;
+
+import android.os.Environment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPrev, btnNext;
+    myPictureView myPicture;
+    TextView tvNumber;
+    int curNum=0;
+    File[] imageFiles = new File[0];
+    String imageFname;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("간단 이미지 뷰어 (변경)");
+        ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        tvNumber = (TextView) findViewById(R.id.tvNumber);
+        myPicture = (myPictureView) findViewById(R.id.myPictureView1);
+
+        File[] allFiles = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath()+"/Pictures").listFiles();
+        for (int i=0; i<allFiles.length; i++)
+            if (allFiles[i].isFile()) {
+                imageFiles = Arrays.copyOf(imageFiles, imageFiles.length + 1);
+                imageFiles[imageFiles.length-1] = allFiles[i];
+            }
+        imageFname = imageFiles[curNum].toString();
+        myPicture.imagePath = imageFname;
+        tvNumber.setText("1" + "/" + (imageFiles.length));
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum <= 0) {
+                    curNum = imageFiles.length - 1;
+                } else {
+                    curNum--;
+                }
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                tvNumber.setText((curNum+1) + "/" + (imageFiles.length));
+
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum >= imageFiles.length - 1) {
+                    curNum = 0;
+                } else {
+                    curNum++;
+                }
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                tvNumber.setText( (curNum+1) + "/" + (imageFiles.length));
+            }
+        });
+
+    }
+
+}
